d19b75d76671657826bd3c1db492c63a64dd4eca
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-01-24 10:35:15 -0600

    Fizzbuzz feature with whizz

--- FizzBuzz/Ruby/fizzbuzz.rb
+++ FizzBuzz/Ruby/fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz 
   numbers = []
   (1..100).each do |i|
-    numbers[i] = i if (i % 3 != 0 && i % 5 != 0) 
+    numbers[i] = i if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) 
     numbers[i] = numbers[i].to_s + 'fizz' if i % 3 == 0
     numbers[i] = numbers[i].to_s + 'buzz' if i % 5 == 0 
+    numbers[i] = numbers[i].to_s + 'whizz' if i % 7 == 0 
   end
   numbers
 end
--- FizzBuzz/Ruby/fizzbuzz_test.rb
+++ FizzBuzz/Ruby/fizzbuzz_test.rb
@@ -19,6 +19,14 @@ describe "#fizzbuzz" do
     it{ expect(array[100]).to eq 'buzz'}
   end
 
+  context "When the number is multiple of 7" do
+    it{ expect(array[7]).to eq 'whizz'}
+    it{ expect(array[14]).to eq 'whizz'}
+    it{ expect(array[49]).to eq 'whizz'}
+    it{ expect(array[91]).to eq 'whizz'}
+    it{ expect(array[77]).to eq 'whizz'}
+  end
+
   context "When the number is multiple of 3 and 5" do
     it{ expect(array[15]).to eq 'fizzbuzz'}
     it{ expect(array[30]).to eq 'fizzbuzz'}
@@ -27,11 +35,22 @@ describe "#fizzbuzz" do
     it{ expect(array[75]).to eq 'fizzbuzz'}
   end
 
+  context "When the number is multiple of 3 and 7" do
+    it{ expect(array[21]).to eq 'fizzwhizz'}
+    it{ expect(array[42]).to eq 'fizzwhizz'}
+    it{ expect(array[63]).to eq 'fizzwhizz'}
+  end
+
+  context "When the number is multiple of 5 and 7" do
+    it{ expect(array[35]).to eq 'buzzwhizz'}
+    it{ expect(array[70]).to eq 'buzzwhizz'}
+  end
+
   context "When the number is not multiple of 3 and 5" do
     it{ expect(array[1]).to eq 1}
-    it{ expect(array[7]).to eq 7}
-    it{ expect(array[77]).to eq 77}
+    it{ expect(array[11]).to eq 11}
+    it{ expect(array[71]).to eq 71}
     it{ expect(array[82]).to eq 82}
-    it{ expect(array[98]).to eq 98}
+    it{ expect(array[94]).to eq 94}
   end
 end
